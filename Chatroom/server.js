9338acb17bb7fd08ac33d55f67e03cff4247a166
unknown <daniel.byrne@SC-PC027.ad.salecycle.com>
2015-05-28 14:34:53 +0100

    First checking of node-JS / socket IO powered chatroom. Watch out Yahoo.

---
+++ Chatroom/server.js
@@ -0,0 +1,34 @@
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket) {
+    console.log('user connected');
+
+    socket.on('message', function (msg) {
+        console.log('received chat message', msg);
+        io.emit('message',{
+            message: msg.message,
+            nickname: msg.nickname
+        });
+    });
+
+    socket.on('loginReq', function (nick) {
+        console.log('login request for', nick);
+        
+        //todo, some verification here
+        io.emit('message', {
+            'nickname': 'system',
+            'message': nick + ' has entered the room'
+        });
+
+        io.emit('loginResp', {
+            'status' : 'ok',
+            'nickname': nick
+        });
+    });
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
---
+++ ChatroomServer/Scripts/client.js
@@ -0,0 +1,63 @@
+﻿var socket = io("ws://sc-pc027:3000");
+var nickname = "guest" + Math.floor((Math.random() * 1000) + 1);
+
+// send message
+var sendMessage = function() {
+    var message = $('.textinput').val();
+    console.log('sending message', message);
+
+    socket.emit('message', {
+        message: message,
+        nickname: nickname
+    });
+
+    $('.textinput').val("");
+};
+
+// send login request
+var login = function () {
+    var requestedLogin = $('input.nickname').val();
+    console.log('logging in as ' + requestedLogin);
+
+    socket.emit('loginReq', requestedLogin);
+
+    // login response
+    socket.on('loginResp', function (msg) {
+        console.log('login response', msg);
+
+        if (msg.status == 'ok' && msg.nickname == requestedLogin) {
+            nickname = msg.nickname;
+            $('div.login-container').hide();
+            $('div.room-container').show();
+        }
+    });
+};
+
+// enter key press
+$('input.textinput').on('keypress', function(ev) {
+    if (ev.keyCode == 13) {
+        sendMessage();
+    }
+});
+$('input.nickname').on('keypress', function(ev) {
+    if (ev.keyCode == 13) {
+        login();
+    }
+});
+
+// submit button click
+$('button.submit').on('click', sendMessage);
+
+// login button click
+$('button.login').on('click', login);
+
+// receive message event handler
+socket.on('message', function (msg) {
+    console.log('received chat message', msg);
+
+    $('div.room ul.messages').append($('<li>').text(msg.nickname + ' - ' + msg.message));
+
+    // autoscroll biaches
+    var room = $('div.room')[0];
+    room.scrollTop = room.scrollHeight;
+});
